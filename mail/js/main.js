bc326739ff94bc0e6f9423338ab62fbd3f578440
Riccardo <lupinu.ric@gmail.com>
2022-07-07 17:22:57 +0200

    Project: Fine progetto

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,17 +1,21 @@
 const emailArray = ['simonegiusti@gmail.com', 'alessiovietri@gmail.com', 'jacopodamiani@gmail.com', 'adrianogrimaldi@gmail.com'];
-
+let success = false;
 document.getElementById('send').addEventListener('click', 
 function() { 
         for (let x = 0; x < emailArray.length; x++) {
             let yourEmailDom = document.getElementById('yourEmail');
-            yourEmail = yourEmailDom.value;
+            let yourEmail = yourEmailDom.value;
             if (emailArray[x] == yourEmail) {
-                let yourMessageDom = document.getElementById('yourMessage');
-                yourMessage = yourMessageDom.innerHTML = 'Puoi Entrare!';;
-                yourMessage.value;
-            } else {
-                yourMessage.innerHTML = 'Non Puoi Entrare!';
+                success = true;
             }
+        }
+
+        let yourMessageDom = document.getElementById('yourMessage');
+
+        if (success == true) {
+            yourMessageDom.innerHTML = 'Puoi entrare!'
+        } else {
+            yourMessageDom.innerHTML = 'Non puoi entrare!'
         }
     }  
 );
